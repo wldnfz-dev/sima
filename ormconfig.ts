05568bb9d1dd09f452ea96655d3768d0354cd5c8
wldnfz-dev <wildanafza@gmail.com>
2023-11-10 11:33:58 +0700

    Update

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,12 +3,12 @@ import { DataSource } from "typeorm";
 
 export const connectionSource = new DataSource({
     // driver: mysqlDriver,
-    type : "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "sima",
+    type : "postgres",
+    host: "ec2-3-230-24-12.compute-1.amazonaws.com",
+    port: 5432,
+    username: "szzrdmckqyjosi",
+    password: "b0e5c42f587f3c418ff37da77bc72086d2a66cabf02d1246f92cc1aa8381194d",
+    database: "d10ftl92k1m0at",
     entities : ["dist/**/*.entity{.ts,.js}"],
     synchronize : true,
     migrations: [
