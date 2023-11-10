76503f9320648277091d220d28d63f41445e60c0
wldnfz-dev <wildanafza@gmail.com>
2023-11-10 14:08:40 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { connectionSource } from 'src/ormconfig';
+import { connectionSource } from 'ormconfig';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
